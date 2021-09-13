b42709eae88aa67ac6e50013cd2c2c16c9fe4b20
LEYAMASA <78112723+LEYAMASA@users.noreply.github.com>
2021-09-13 15:55:52 -0700

    Lab 5 Progress Part 1
    Lab 5 in progress

---
+++ lab5/js/lab.js
@@ -0,0 +1,17 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author Leo Yamasaki.
+ * @since  2021.13.9
+ */
+
+ // Declare variables to hold the make, model, color, and year of your vehicle, example, var year = 2004
+var make = "Nissan";
+var model = "Nissan Versa";
+var color = "Gray";
+var year = 2015;
+var extras = "Stormtrooper Handwheel, Blind Spot Mirrors";
