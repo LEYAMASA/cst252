182c78fa7d540cecc041977d1510041b1994feb5
lyamasaki-CSUMB <78112723+LEYAMASA@users.noreply.github.com>
2021-09-13 12:08:14 -0700

    Lab 4 Complete
    Lab 4 Upload

---
+++ lab4/js/lab.js
@@ -0,0 +1,33 @@
+/**
+ * Summary. (use period)
+ *
+ * Description. (use period)
+ *
+ * @link   URL
+ * @file   This files defines the MyClass class.
+ * @author AuthorName.
+ * @since  x.x.x
+ */
+
+/** jshint {inline configuration here} */
+// Create a UI for the player to use
+    // Create buttons and icons
+    // Create a minimap
+    // Program economy resources
+// Create a custom mouse cursor
+    // Program left, right, and scroll mouse button commands
+    // Create right click interface
+    // Create drag select features
+    // Align in-game camera to function with the mouse
+// Create a custom map
+    // Create low poly textured object environment
+// Customize units within the map
+    // Input numerical values for unit stats
+// Place map props for gameplay
+// Program Lua Script for Enemy AI Behavior
+  // Create Defense Plan
+    // Create Blockade Fleet
+  // Create Attack Plan
+    // Create Rampage Mode
+// Send Tester Build for Testers to debug
+  // Check for successful upload onto the P4V Server
