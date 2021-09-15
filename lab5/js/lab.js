d879c980e8e81d178be200068930be4d5953d76b
lyamasaki-CSUMB <78112723+LEYAMASA@users.noreply.github.com>
2021-09-15 10:58:17 -0700

    Lab 5 Complete
    Lab 5 Done

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -15,3 +15,19 @@ var model = "Nissan Versa";
 var color = "Gray";
 var year = 2015;
 var extras = "Stormtrooper Handwheel, Blind Spot Mirrors";
+
+// Declare a boolean variable ownIt for whether you own it or not.
+var ownIt = true;
+
+// Create a new variable age that subtracts the year from the current year
+var age = new Date().getFullYear();
+
+// Use document.writeln() to neatly output each of your variables, example, document.writeln("Make: " + make + "<br>");
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Extras: " + extras + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Own it: " + ownIt + "<br>");
+document.writeln("Age: " + age + "<br>");
+
+document.writeln("");
