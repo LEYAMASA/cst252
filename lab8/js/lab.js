472f57fb6bfac9ef17d3c23a0899052d2fcc17d9
lyamasaki-CSUMB <78112723+LEYAMASA@users.noreply.github.com>
2021-10-12 10:47:52 -0700

    Lab 8 Finished
    Lab 8 Done

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -7,27 +7,27 @@
 var outputEl = document.getElementById("output");
 
 function add5(n){
-  return (n + 5);
+  return n + 5;
 }
-
+// test function
 console.log("5 + 1 = ", add5 + 1);
 
 array = [1, 2, 3, 4, 5];
 console.log("My array: ", array);
-
+// add array to document
 var newEl = document.createElement("p");
 newEl.innerHTML = "Original Numbers:" + JSON.stringify(array);
 outputEl.appendChild(newEl);
 
 var result = array.map(add5);
 console.log("Adding Fives to the array:", result);
-
+// add array to document
 var newEl = document.createElement("p");
 newEl.innerHTML = "New Fives:" + JSON.stringify(result);
 outputEl.appendChild(newEl);
 
 var result = array.map(function(n){
- return "Hello Number " + n;
+ return n + 5;
 })
 
 console.log("Introducing each number:", result);
