64dcee50a7cadb82a768411eb778f485020b09cd
lyamasaki-CSUMB <78112723+LEYAMASA@users.noreply.github.com>
2021-10-12 12:09:50 -0700

    Lab 9 Upload
    Lab 9 Finished

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -4,36 +4,41 @@
  *
  * License: Public Domain
  **/
+ window.onload = function () {
 
-    // your script here
+     // your script here
 
-   // Use getElementById() to find your output <div> and assign it to a variable outputEl (step 1)
-  var outputEl = document.getElementById("output");
+    // Use getElementById() to find your output <div> and assign it to a variable outputEl (step 1)
+    var outputEl = document.getElementById("output");
+    console.log("outputEl: ", outputEl);
 
-   // Create a new element with document.createElement("p") and assign it to a variable new1El (step 2)
-   var oneEl = document.createElement("button");
-   var twoEl = document.createElement("button");
+    // Create a new element with document.createElement("p") and assign it to a variable new1El (step 2)
+    var oneEl = document.createElement("p");
 
-   // Change the html attribute of new1El to say "something new.""
-   oneEl.id = "new-one"
-   oneEl.innerHTML = "Something old.";
-   // Create a new element with document.createElement("p") and assign it to a variable new1El
-   twoEl.id = "new-two";
-   twoEl.innerHTML = "Something new.";
-   // Change the html attribute of new1El to say "something else.""
-   document.body.appendChild(button);
+    // Change the html attribute of new1El to say "something new.""
+    oneEl.innerHTML = "Original Me.";
+    oneEl.id = "new-one";
 
-   // Append both new elements one at a time using appendChild() (step 3)
-   outputEl.appendChild(oneEl);
-   outputEl.appendChild(twoEl);
+    // Create a new element with document.createElement("p") and assign it to a variable new1El
+    var twoEl = document.createElement("p");
+    twoEl.id = "new-two";
 
-   // Change the css attributes of at least two elements of your page
-   document.getElementById("body").style.color="blue";
-   document.getElementById("div").style.position = "absolute";
+    // Change the html attribute of new1El to say "something else.""
+    twoEl.innerHTML = "Newest Me.";
 
-   // we do some stuff
-   // new1El.style.color = "blue";
-   // outputEl.style.border = "dashed 1px red";
-   outputEl.className = "changed";
+    // Append both new elements one at a time using appendChild() (step 3)
+    outputEl.appendChild(oneEl);
+    outputEl.appendChild(twoEl);
 
-}
+    // Change the css attributes of at least two elements of your page
+    // we do some stuff
+    // new1El.style.color = "blue";
+    // outputEl.style.border = "dashed 1px red";
+    oneEl.style.backgroundColor = "chartreuse"
+    oneEl.style.color = "black";
+    oneEl.style.fontSize = "100px";
+    twoEl.style.backgroundColor = "darkorchid"
+    twoEl.style.color = "white";
+    twoEl.style.fontSize = "100px";
+
+ }
