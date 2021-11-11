25ab5d82af971202c3a378ae90664c0d4d787c64
lyamasaki-CSUMB <78112723+LEYAMASA@users.noreply.github.com>
2021-11-11 10:34:18 -0800

    Lab 16 Finished
    Lab 16 Done

--- lab16/js/lab.js
+++ lab16/js/lab.js
@@ -4,51 +4,19 @@
  *
  * License: Public Domain
  **/
+function Vehicle(make, model, year, color, extras, name){
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.color = color;
+  this.extras = extras;
+  this.name = name;
+  this.info = function() {
+    var str = "It's a " + color + " " + year + " " + make + " " + model + " with " + extras + " called " + name + ".";
+    return str
+  }
 
- // print all the numbers from 1 to 100.
- //for (var num=1; num <= 50; num++) {
+}
 
-   // // For numbers which are multiples of both 3 and 5, print “FizzBuzz”
-   // if (num % 15 == 0) {
-   //   console.log("FizzBuzz");
-   // }
-   // // If the number is a multiple of 3, print “Fizz” instead of the number.
-   // else if (num % 3 == 0) {
-   //   console.log("Fizz");
-   // }
-   // // If the number is a multiple of 5, print “Buzz” instead of the number.
-   // else if (num % 5 == 0) {
-   //   console.log("Buzz");
-   // }
-   // else {
-   //   console.log(num)
-   // }
-
-//   var outputStr = ""
-   // if multiple of 3, add fizz
-//   if (num % 3 == 0) {
-//     outputStr += "Fizz"
-//     $("#output").append("<p>" + outputStr);
-//   }
-//   // if multiple of 5, add buzz
-//   if (num % 5 == 0) {
-//     outputStr += "Buzz"
-//     $("#output").append("<p>" + outputStr);
-//   }
-//   // if multiple of 7, add boom
-//   if (num % 7 == 0) {
-//     outputStr += "Boom"
-//     $("#output").append("<p>" + outputStr);
-//   }
-//   // if string is still blank, print it
-//   if (outputStr == "") {
-//     console.log(num);
-//     $("#output").append("<p>" + outputStr);
-//   }
-//   // otherwise, print str
-//   else {
-//     console.log(outputStr + '!');
-//     $("#output").append("<p>" + outputStr);
-//   }
-
- }
+var car = new Vehicle("Nissan", "Versa", "2016", "Gray", "Blind Spot Mirrors", "Bertha");
+$("#output").append("<p>" +  car.info() + "</p>");
